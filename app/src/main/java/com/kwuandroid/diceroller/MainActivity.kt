02c29ddad38cef87d421f51081f16eda345beaac
minho2618 <minho2618@naver.com>
2022-01-19 15:06:14 +0900

    Test

---
+++ app/src/main/java/com/kwuandroid/diceroller/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.kwuandroid.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById(R.id.roll_button)
+        rollButton.setOnClicker
+    }
+
+    private fun rollDice() {
+
+    }
+
+}
